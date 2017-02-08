7f113364401f1c1e334064c75ff19c404533a9cf
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-02-08 15:02:11 -0800

    ascii art and stuff

--- logic.js
+++ logic.js
@@ -1,3 +1,15 @@
+/*
+ _____  ____  ____   ___        ______  __ __    ___       ___   ___    ___        ____    ____  _      _
+|     |l    j|    \ |   \      |      T|  T  T  /  _]     /   \ |   \  |   \      |    \  /    T| T    | T
+|   __j |  T |  _  Y|    \     |      ||  l  | /  [_     Y     Y|    \ |    \     |  o  )Y  o  || |    | |
+|  l_   |  | |  |  ||  D  Y    l_j  l_j|  _  |Y    _]    |  O  ||  D  Y|  D  Y    |     T|     || l___ | l___
+|   _]  |  | |  |  ||     |      |  |  |  |  ||   [_     |     ||     ||     |    |  O  ||  _  ||     T|     T
+|  T    j  l |  |  ||     |      |  |  |  |  ||     T    l     !|     ||     |    |     ||  |  ||     ||     |
+l__j   |____jl__j__jl_____j      l__j  l__j__jl_____j     \___/ l_____jl_____j    l_____jl__j__jl_____jl_____j
+
+*/
+
+// ----------------------------------- SVG and BACKGROUND --------------------------------------------
 var height = 300
 var width = 800
 
@@ -10,7 +22,10 @@ var canvas = svg.append('rect')
 			.attr('width', '100%')
 			.attr('height', '100%')
 			.attr('fill', 'blue')
+// ----------------------------------------------------------------------------------------------
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
+// -----------------------------------  BALLS --------------------------------------------
 var balls = [{0: false},{1: false},{2: false},{3: false},
 				{4: false}, {5: false}, {6: false}, {7: false},
 				{8: false}]
@@ -35,7 +50,10 @@ svgBalls
 		        .on("start", dragstarted)
 		        .on("drag", dragged)
 		        .on("end", dragended));
+// ----------------------------------------------------------------------------------------------
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
+// ----------------------------------- SIMULATION --------------------------------------------
 var simulation = d3.forceSimulation(balls)
 			.force("collide", d3.forceCollide().radius(10))
 			.force('y', d3.forceY(250).strength(0.3))
@@ -49,6 +67,10 @@ var ticked = function() {
 simulation
 	.on("tick", ticked);
 
+// ----------------------------------------------------------------------------------------------
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+// ----------------------------------- DRAGGING --------------------------------------------
 function dragstarted(d) {
 	simulation.restart();
     simulation.alpha(1.0);
@@ -69,23 +91,35 @@ function dragended(d) {
 		.classed("active", false);
 	simulation.alphaTarget(0.1);
 }
+// ----------------------------------------------------------------------------------------------
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
+// ----------------------------------- USING SCALES --------------------------------------------
 function useScales(){
 	var inTheRightScale = false;
 	var inTheLeftScale = false;
 
+	var numOfBallsOnLeft = 0;
+	var numOfBallsOnRight = 0;
+
 	var onLeft = svgBalls
 			.filter(function(d, i){
-				if(d.x > 100 && d.x < 180 && d[i] === true){
-					inTheLeftScale = true
+				if(d.x > 100 && d.x < 180){
+					numOfBallsOnLeft ++
+					if(d[i] === true){
+						inTheLeftScale = true
+					}
 				}
 				return d.x > 100 && d.x < 180
 			})
 
 	var onRight = svgBalls
 			.filter(function(d, i){
-				if(d.x > 240 && d.x < 320 && d[i] === true){
-					inTheRightScale = true
+				if(d.x > 240 && d.x < 320){
+					numOfBallsOnRight ++
+					if(d[i] === true){
+						inTheRightScale = true
+					}
 				}
 				return d.x > 240 && d.x < 320
 			})
@@ -112,8 +146,7 @@ function useScales(){
 				.attr("transform", "translate(0, -110)")
 
 	}
-
-	if(inTheRightScale){
+	else if(inTheRightScale){
 		leftScales
 			.transition()
 				.duration(500)
@@ -135,16 +168,34 @@ function useScales(){
 				.attr("transform", "translate(0, -90)")
 
 	}
-		console.log("Is in left? ", inTheLeftScale)
-		console.log("Is in right? ", inTheRightScale)
+	else{
+		leftScales
+			.transition()
+				.duration(500)
+				.attr("transform", "translate(0, -100)")
 
+		rightScales
+			.transition()
+				.duration(500)
+				.attr("transform", "translate(0, -100)")
+
+		onLeft
+			.transition()
+				.duration(500)
+				.attr("transform", "translate(0, -100)")
+
+		onRight
+			.transition()
+				.duration(500)
+				.attr("transform", "translate(0, -100)")
+	}
+
+	console.log("On the left side: " + numOfBallsOnLeft + ". On the right side: " + numOfBallsOnRight)
 }
+// ----------------------------------------------------------------------------------------------
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-function isThere(b){
-
-	b.forEach( ball => console.log(ball) )
-}
-
+//---------------------------------DRAWING SCALES -----------------------------------------------
 var rightScalesCoordinartes = [[{x: 340, y:250}, {x: 420, y:250}, {x:410,y:260}, {x:350,y:260}]];
 
 var d3DataRightScales = rightScalesCoordinartes.map(function(point){
@@ -178,5 +229,5 @@ leftScales
     .style("stroke", "#FFFCD3")
     .style("fill", '"orange')
     .attr("points", (d) =>{ return d});
-
-console.log(balls)
+//-----------------------------------------------------------------------------------------------
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
