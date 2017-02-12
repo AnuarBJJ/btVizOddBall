78e32350ba356287c4271c325c65e0d4bd96caf1
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-02-12 13:31:10 -0800

    logic fixes

--- logic.js
+++ logic.js
@@ -34,13 +34,15 @@ var canvas = svg.append('rect')
 //   .attr('x', -150)
 //   .attr('y', 100)
 
+var binCoordinates = {x: 715, y: 340}
+
  var bin =   svg
   .append('image')
   .attr('xlink:href','binBlueBG.jpg')
   .attr('height', '80')
   .attr('width', '80')
-  .attr('x', 715)
-  .attr('y', 340)
+  .attr('x', binCoordinates.x)
+  .attr('y', binCoordinates.y)
 //-----------------------------------------------------------------------------------------------
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
@@ -58,6 +60,42 @@ copper.append("stop").attr("offset", "0%").style("stop-color", "#655a5a");
 copper.append("stop").attr("offset", "100%").style("stop-color", "gray");
 
 // ----------------------------------------------------------------------------------------------
+// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+//---------------------------------INFO BOARD-----------------------------------------------
+var numOfBallsOnLeftFirst = 0;
+var numOfBallsOnRightFirst = 0;
+
+var numOfBallsOnLeftSecond = 0;
+var numOfBallsOnRightSecond = 0;
+
+var message = [{text:"SCALES USAGES LEFT:", x: 50, y: 50, size: 30, fill: "#cc0000"}, //0
+				{text: "2", x: 320, y: 50, size: 35, fill: "#cc0000"},				  //1
+				{text: 'left side', x: 300, y: 90, size: 25, fill: 'black'},          //2
+				{text: 'right side', x: 450, y: 90, size: 25, fill: 'black'},         //3
+				{text: 'sorted out', x: 600, y: 90, size: 25, fill: 'black'},         //4
+				{text: 'first usage stats: ', x: 80, y: 120, size: 25, fill: 'black'},//5
+				{text: 'second usage stats: ', x: 80, y: 170, size: 25, fill: 'black'}, //6
+				{text: 0, x: 320, y: 120, size: 25, fill: 'black'}, //7
+				{text: 0, x: 470, y: 120, size: 25, fill: 'black'}, //8
+				{text: 0, x: 320, y: 170, size: 25, fill: 'black'}, //9
+				{text: 0, x: 470, y: 170, size: 25, fill: 'black'}, //10
+				{text: 'second usage stats: ', x: 80, y: 170, size: 25, fill: 'black'},  //11
+				{text: 'second usage stats: ', x: 80, y: 170, size: 25, fill: 'black'},  //12
+				]
+
+
+var legend = svg.selectAll("text")
+		.data(message)
+		.enter()
+		.append("text")
+		.text(function(d){return d.text})
+		.attr('x', d => d.x)
+		.attr('y', d => d.y)
+		.attr('font-size', d => d.size)
+		.attr('fill', d => d.fill)
+
+//-----------------------------------------------------------------------------------------------
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 // -----------------------------------  BALLS --------------------------------------------
@@ -92,13 +130,15 @@ svgBalls
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 // ----------------------------------- SIMULATION --------------------------------------------
+var shiftRight = 180;
+
 var simulation = d3.forceSimulation(balls)
 			.force("collide", d3.forceCollide().radius(16))
 			.force('y', d3.forceY(400).strength(0.555))
 
 var ticked = function() {
      svgBalls
-     	.attr("cx", function(d, i){ return d.x + 180;})
+     	.attr("cx", function(d, i){ return d.x + shiftRight;})
         .attr("cy", function(d){ return d.y;})
 }
 
@@ -130,7 +170,7 @@ function dragended(d, i) {
 	// 	.classed("active", false);
 	// console.log("Before remove: :", balls)
 
-	if(d.x > 480){
+	if(d.x + shiftRight > binCoordinates.x){
 		balls = balls.filter(d => d.id != i);
 
 		var update = svg.selectAll('circle')
@@ -154,40 +194,43 @@ function dragended(d, i) {
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 // ----------------------------------- USING SCALES --------------------------------------------
-var numOfBallsOnLeft = 0;
-var numOfBallsOnRight = 0;
 
-function useScales(){
-	message[1]['text'] = parseInt(message[1]['text'])-1;
-	legend.data(message).text(function(d){return d.text;})
+var rightScalesLeftSide = 550;
+var rightScalesRigthSide = 710;
+
+var leftScalesLeftSide = 340;
+var leftScalesRigthSide = 510;
+
+function useScales(numOfBallsOnLeft, numOfBallsOnRight){
+
+	var numOfBallsOnLeft = 0
+	var numOfBallsOnRight = 0
 
 	var text = d3.selectAll('text').data(message)
 	console.log(parseInt(message[1]['text']))
 	var inTheRightScale = false;
 	var inTheLeftScale = false;
 
-
-
 	var onLeft = svgBalls
 			.filter(function(d, i){
-				if(d.x > 100 && d.x < 180){
+				if(d.x > leftScalesLeftSide-shiftRight && d.x < leftScalesRigthSide-shiftRight){
 					numOfBallsOnLeft ++
 					if(d['odd'] === true){
 						inTheLeftScale = true
 					}
 				}
-				return d.x > 100 && d.x < 180
+				return d.x > leftScalesLeftSide-shiftRight && d.x < leftScalesRigthSide-shiftRight
 			})
 
 	var onRight = svgBalls
 			.filter(function(d, i){
-				if(d.x > 240 && d.x < 320){
+				if(d.x > rightScalesLeftSide-shiftRight && d.x < rightScalesRigthSide-shiftRight){
 					numOfBallsOnRight ++
 					if(d['odd'] === true){
 						inTheRightScale = true
 					}
 				}
-				return d.x > 240 && d.x < 320
+				return d.x > rightScalesLeftSide-shiftRight && d.x < rightScalesRigthSide-shiftRight
 			})
 
 	function skewLeft(){
@@ -257,6 +300,8 @@ function useScales(){
 					.attr("transform", "translate(0, -50)")
 	}
 
+
+	console.log(numOfBallsOnLeft, numOfBallsOnRight)
 	if(numOfBallsOnLeft === numOfBallsOnRight){
 		if(inTheLeftScale){
 			skewLeft()
@@ -272,6 +317,57 @@ function useScales(){
 	} else if (numOfBallsOnLeft < numOfBallsOnRight){
 		skewRight()
 	}
+
+	if(parseInt(message[1]['text']) === 2){
+		setTimeout(function(){ alert("Toss non-odd balls in the bin."); }, 1000);
+
+		document.getElementById('step2').style.visibility = 'visible'
+		document.getElementById('use').style.visibility = 'hidden'
+
+		message[7]['text'] = numOfBallsOnLeft
+		message[8]['text'] = numOfBallsOnRight
+	}
+	if(parseInt(message[1]['text']) === 1){
+		setTimeout(function(){ alert("Toss non-odd balls in the bin."); }, 1000);
+
+		document.getElementById('step2').style.visibility = 'visible'
+		document.getElementById('use').style.visibility = 'hidden'
+
+		message[9]['text'] = numOfBallsOnLeft
+		message[10]['text']  = numOfBallsOnRight
+	}
+
+	message[1]['text'] = parseInt(message[1]['text'])-1;
+
+
+	if(parseInt(message[1]['text']) < 1){
+		message[1]['text'] = "over the limit"
+		setTimeout(function(){ alert("Game over");}, 1000);
+	}
+	var update = svg.selectAll('text')
+			.data(message)
+			.text(d=>d.text)
+
+	// var enter = update
+	// 	.enter()
+	// 	.append('text')
+	// 	.text(d => d.text)
+	// 	.attr('x', d => d.x)
+	// 	.attr('y', d => d.y)
+	// 	.attr('font-size', d => d.size)
+	// 	.attr('fill', d => d.fill)
+
+	// console.log(enter)
+
+	// update.exit().remove()
+
+	// update.merge(enter)
+	// 	.text(function(d){
+	// 		console.log(d);
+	// 		return d.text;
+	// 	})
+
+	// update.enter().text(function(d){return d.text;})
 }
 // ----------------------------------------------------------------------------------------------
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
@@ -279,8 +375,9 @@ function useScales(){
 //---------------------------------DRAWING SCALES -----------------------------------------------
 var rightScales = svg.append('g')
 var leftScales = svg.append('g')
+
 function drawScales(){
-	var rightScalesCoordinartes = [[{x: 550, y:410}, {x: 710, y:410}, {x:690,y:420}, {x:570,y:420}]];
+	var rightScalesCoordinartes = [[{x: rightScalesLeftSide, y:410}, {x: rightScalesRigthSide, y:410}, {x:rightScalesRigthSide-20,y:420}, {x:rightScalesLeftSide+20,y:420}]];
 
 	var d3DataRightScales = rightScalesCoordinartes.map(function(point){
 	    return point.reduce(function(s, n){return s + ', ' + n.x + ',' + n.y}, '').substr(2)
@@ -295,7 +392,7 @@ function drawScales(){
 	    .style("fill", 'url(#copper)')
 	    .attr("points", (d) =>{ return d});
 
-	var leftScalesCoordinartes = [[{x: 340, y:410}, {x: 510, y:410}, {x:490,y:420}, {x:360,y:420}]];
+	var leftScalesCoordinartes = [[{x: leftScalesLeftSide, y:410}, {x: leftScalesRigthSide, y:410}, {x:leftScalesRigthSide-20,y:420}, {x:leftScalesLeftSide+20,y:420}]];
 
 	var d3DataLeftScales = leftScalesCoordinartes.map(function(point){
 	    return point.reduce(function(s, n){return s + ', ' + n.x + ',' + n.y}, '').substr(2)
@@ -316,37 +413,12 @@ drawScales()
 //-----------------------------------------------------------------------------------------------
 // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-//---------------------------------INFO BOARD-----------------------------------------------
-var message = [{text:"SCALES USAGES LEFT:", x: 50, y: 50, size: 30, fill: "#cc0000"},
-				{text: "2", x: 320, y: 50, size: 35, fill: "#cc0000"},
-				{text: 'left side', x: 300, y: 90, size: 25, fill: 'black'},
-				{text: 'right side', x: 450, y: 90, size: 25, fill: 'black'},
-				{text: 'sorted out', x: 600, y: 90, size: 25, fill: 'black'},
-				{text: 'first usage stats: ', x: 80, y: 120, size: 25, fill: 'black'},
-				{text: 'second usage stats: ', x: 80, y: 170, size: 25, fill: 'black'},
-				{text: numOfBallsOnLeft, x: 320, y: 120, size: 25, fill: 'black'},
-				{text: numOfBallsOnRight, x: 470, y: 120, size: 25, fill: 'black'},
-				{text: 'second usage stats: ', x: 80, y: 170, size: 25, fill: 'black'},
-				{text: 'second usage stats: ', x: 80, y: 170, size: 25, fill: 'black'},
-				]
-
-
-var legend = svg.selectAll("text")
-		.data(message)
-		.enter()
-		.append("text")
-		.text(function(d){return d.text})
-		.attr('x', d => d.x)
-		.attr('y', d => d.y)
-		.attr('font-size', d => d.size)
-		.attr('fill', d => d.fill)
-
-//-----------------------------------------------------------------------------------------------
-// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-
 //---------------------------------STEP 2-----------------------------------------------
 function readyForStepTwo(){
 	console.log('almost ready')
+
+	document.getElementById('use').style.visibility = 'visible'
+	document.getElementById('step2').style.visibility = 'hidden'
 
 	svg.selectAll('circle')
 		.filter(d => d.x > 100)
